bce1977b8fe97a54613e7ce8149bac91ca1925a4
mohit <mohitklamba@gmail.com>
2021-05-22 16:12:50 +0530

    halfway

---
+++ regex.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/things/:id([0-9]{5})', function(req, res){
+   res.send('id: ' + req.params.id);
+});
+
+//Other routes here
+app.get('*', function(req, res){
+    res.send('Sorry, this is an invalid URL.');
+ });
+
+app.listen(3000);
